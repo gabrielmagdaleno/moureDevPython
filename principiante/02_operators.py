873b2ac2ba589a8dfae92e3cbbf5772686c10666
Gabriel Magdaleno <gmagdaleno1981@gmaiil.com>
2025-01-22 23:06:35 +0100

    inicio curso mouredev hasta 2_operadores

---
+++ principiante/02_operators.py
@@ -0,0 +1,68 @@
+# #Operadores
+
+# print(2 + 3) # Suma
+# print(2 - 3) # Resta
+# print(2 * 3) # Multiplicación
+# print(2 / 3) # División
+# print(2 // 3) # División entera
+# print(2 % 3) # Módulo
+# print(2 ** 3) # Exponenciación
+# print(2 ** 0.5) # Raíz cuadrada
+# print(2 ** (1/3)) # Raíz cúbica
+
+# print("Hola" + " " + "Mundo " +  "Python") # Concatenación
+# print("Hola " * 3) # Repetición
+
+# my_float = 2.5 * 2
+# print("Hola " * int(my_float)   + "Python") # Repetición
+
+# # Operadores de comparación
+
+# print(2 == 3) # Igualdad    
+# print(2 != 3) # Diferente   
+# print(2 < 3) # Menor que    
+# print(2 > 3) # Mayor que
+# print(2 <= 3) # Menor o igual que
+# print(2 >= 3) # Mayor o igual que
+
+# # Operadores lógicos
+
+# print(True and False) # Y
+# print(True or False) # O
+# print(not True) # Negación
+# print(not False) # Negación
+
+# # Operadores de asignación
+
+# a = 2
+# a += 3 # a = a + 3
+# print(a)
+# a -= 3 # a = a - 3
+# print(a)
+# a *= 3 # a = a * 3
+# print(a)
+
+# # Operadores de identidad
+
+# a = 2
+# b = 2
+# print(a is b) # Identidad
+# print(a is not b) # No identidad
+
+# # Operadores de comparación de ordenación alfabética
+print("Hola" == "Python") # Igualdad    
+print("Hola" != "Python") # Diferente   
+print("Hola" < "Pyhon") # Menor que    
+print("Hola" > "Python") # Mayor que
+print("Hola"<= "Python") # Menor o igual que
+print("Hola" >= "Python") # Mayor o igual que
+print (len("Hola")>= len("Python"))
+
+# Operadores de logicos
+
+print(2 > 3 and "Hola" > "Python") 
+print(2 > 3 or "Hola" > "Python") 
+print(2 < 3 and "Hola" > "Python") 
+print(2 < 3 and "Hola" > "Python")
+print(2 < 3 or ("Hola" > "Python" and 2 == 2))
+print(not(2 > 3))   
